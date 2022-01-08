27b508669072ad68082f023edc37b5cbc86fe97c
Ian de Jesus <ian@eskwelabs.com>
2022-01-08 19:56:09 +0900

    feat: make graphql request in content script
    JIRA: https://insiderinvesting.atlassian.net/browse/INSD-98

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,35 @@
+import { createElement } from 'react';
 import { printLine } from './modules/print';
+import { render } from 'react-dom';
+import { request, gql } from 'graphql-request';
 
+const GET_ACCOUNT_ID = gql`
+  query GetAccountByEmail($email: String!) {
+    getAccountByEmail(email: $email) {
+      id
+    }
+  }
+`;
+const getAccountID = async () => {
+  const res = await request('http://localhost:8000/graphql', GET_ACCOUNT_ID, {
+    email: 'iandjx@gmail.com',
+  }).catch((err) => {
+    console.log(err);
+    return;
+  });
+  console.log(res);
+};
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
 printLine("Using the 'printLine' function from the Print Module");
+
+var container = document.getElementById('main');
+var button = createElement('button', {
+  children: 'hlllo',
+  onClick: () => getAccountID(),
+});
+// button.innerHTML = 'click me';
+// container.appendChild(button);
+
+render(button, container);
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
+import { idText } from 'typescript';
 import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
+
+async function injectTheScript3() {
+  let queryOptions = { active: true, currentWindow: true };
+
+  let [tab] = await chrome.tabs.query(queryOptions);
+
+  chrome.storage.sync.set({ currentURL: tab.url });
+
+  // chrome.runtime.reload();
+  // await chrome.scripting.executeScript({
+  //   target: {
+  //     tabId: tab.id,
+  //   },
+  //   files: ['contentScript.bundle.js'],
+  // });
+}
 
 const Popup = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
+        <button onClick={injectTheScript3}>Hello</button>
       </header>
     </div>
   );
